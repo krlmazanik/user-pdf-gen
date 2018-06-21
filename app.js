7b60242c112a911bd7cf4690eba8cfac2d8ae0ed
User <krlmazanik@gmail.com>
2018-06-21 23:38:53 +0200

    added essential error handler

--- app.js
+++ app.js
@@ -34,35 +34,38 @@ app.post("/generate", (req, res) => {
 
   db.query("SELECT * FROM user WHERE firstName=?", name, (err, rows) => {
     if (err) console.log(err);
+    if (rows.length > 0) {
+      var blob = rows[0].image;
+      const { firstName, lastName, id, pdf } = rows[0];
 
-    var blob = rows[0].image;
-    const { firstName, lastName, id, pdf } = rows[0];
+      if (pdf == null) {
+        var buffer = new Buffer(blob, "binary");
+        var bufferBase64 = buffer.toString("base64");
+        var bufferP = new Buffer(bufferBase64, "base64");
 
-    if (pdf == null) {
-      var buffer = new Buffer(blob, "binary");
-      var bufferBase64 = buffer.toString("base64");
-      var bufferP = new Buffer(bufferBase64, "base64");
+        const doc = new PDFDocument();
 
-      const doc = new PDFDocument();
+        let writeStream = new WritableBufferStream();
+        doc.pipe(writeStream);
 
-      let writeStream = new WritableBufferStream();
-      doc.pipe(writeStream);
+        doc.text(`${firstName} ${lastName}`);
+        doc.image(bufferP);
+        doc.end();
 
-      doc.text(`${firstName} ${lastName}`);
-      doc.image(bufferP);
-      doc.end();
+        writeStream.on("finish", () => {
+          const pdfBlob = writeStream.toBuffer();
+          let sql = "UPDATE user SET pdf = ? Where ID = ?";
 
-      writeStream.on("finish", () => {
-        const pdfBlob = writeStream.toBuffer();
-        let sql = "UPDATE user SET pdf = ? Where ID = ?";
-
-        db.query(sql, [pdfBlob, id], (err, result) => {
-          if (err) console.log(err);
-          res.send(JSON.stringify({ pdfWasCreated: true }));
+          db.query(sql, [pdfBlob, id], (err, result) => {
+            if (err) console.log(err);
+            res.send(JSON.stringify({ pdfWasCreated: true }));
+          });
         });
-      });
+      } else {
+        res.send(JSON.stringify({ pdfWasCreated: false }));
+      }
     } else {
-      res.send(JSON.stringify({ pdfWasCreated: false }));
+      res.render('errorPage', {name: name});
     }
   });
 });
